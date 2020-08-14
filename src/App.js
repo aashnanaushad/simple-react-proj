812ef3c390c442e3278bdf23ee69b411907361f7
aashnanaushad <aashnanaushad@gmail.com>
2020-08-15 03:16:24 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useRoutes } from "hookrouter";
+import LandingPage from "./Components/LandingPage";
+import Create from "./Components/Create";
+import Edit from "./Components/Edit";
+import View from "./Components/View";
+import ViewPost from "./Components/ViewPost";
 
-function App() {
+const routes = {
+  "/": () => <LandingPage />,
+  "/create": () => <Create />,
+  "/view": () => <View />,
+  "/edit/:id": ({ id }) => <Edit id={id} />,
+  "/view/:id": ({ id }) => <ViewPost id={id} />
+
+};
+
+const App = () => {
+  const pages = useRoutes(routes);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative bg-gray-200 min-h-screen pb-24">
+      {pages}
+      {!pages && (
+        <div className="flex justify-center py-16">
+          Error 404: Page not found
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
